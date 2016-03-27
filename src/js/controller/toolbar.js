bd623216b5a3932483090efc17eb19942f0bdb22
Marc Rodriguez <nebrios2@gmail.com>
2016-03-27 07:59:35 +0200

    Lists can be added, checks name is unique

--- src/js/controller/toolbar.js
+++ src/js/controller/toolbar.js
@@ -1,8 +1,17 @@
 'use strict';
 
-module.exports = function($scope, $mdMenu){
+module.exports = function($scope, $mdMenu, $mdDialog, $mdToast){
+	var _ = require('underscore');
 	this.currentList = 'Shopping List';
-	this.lists = ['Shopping List', 'Another List'];
+	this.lists = ['Shopping List', 'A'];
+
+	// var last = {
+	// 	bottom: false,
+	// 	top: true,
+	// 	left: false,
+	// 	right: true
+	// };
+	// $scope.toastPosition = angular.extend({}, last);
 
 	var originatorEv;
 	this.openMenu = function($mdOpenMenu, ev) {
@@ -10,13 +19,45 @@ module.exports = function($scope, $mdMenu){
 		$mdOpenMenu(ev);
 	};
 
+	// Switches current list from the select menu on top
 	this.switchList = function($event, list) {
 		var self = this;
+		// Wait for the menu to close before switching current list
 		$mdMenu.hide().then(function() {
 			self.currentList = list;
 		});
 	};
 
+	// Shows a dialog asking to insert the name for the new list
+	this.showPrompt = function($event) {
+		var self = this;
+		var confirm = $mdDialog.prompt()
+			.title('Name the new list')
+			.textContent('Names can not be repeated')
+			.placeholder('List name')
+			.ariaLabel('List name')
+			.targetEvent($event)
+			.ok('Add')
+			.cancel('Cancel');
+		$mdDialog.show(confirm).then(function(result) {
+			if (_.every(self.lists, function(list) {
+				return result.toLowerCase() != list.toLowerCase();
+			})) {
+				self.lists.push(result);
+				self.currentList = result;
+			}
+			else {
+				$mdToast.show(
+					$mdToast.simple()
+						.textContent('This list already exists!')
+						.position('top right')
+				);
+			}
+		});
+	};
+
+	// Custom filter to select all array elements that do NOT match the pattern
+	// Used to show all the lists except the current one
 	this.listsFilter = function(actual, expected) {
 		return actual != expected;
 	};
